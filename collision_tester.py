7cb0e8abefdb790d2b02e6b854d08ac03aca47ec
Keval Khara <kevalkhara95@gmail.com>
2017-10-09 15:46:56 -0400

    Update collision_tester.py

--- collision_tester.py
+++ collision_tester.py
@@ -25,7 +25,8 @@ def runprogram(program, args, inputstr):
 
 
 class CollisionTestCase(unittest.TestCase):
-    "empty class - write this"
+    
+    #checking if there are no errors for single input
     def test_one(self):
         strin = "one 20 10 -2 1"
         correct_out = "3\none 14 13 -2 1\n"
@@ -34,6 +35,7 @@ class CollisionTestCase(unittest.TestCase):
         self.assertEqual(out,correct_out)
         self.assertEqual(errs,"")
     
+    #checking if there are no errors for multiple inputs
     def test_mult_part_basic(self):
         strin = "one 20 10 -2 1\ntwo 0 0 -1 -1"
         correct_out = "3\none 14 13 -2 1\ntwo -3 -3 -1 -1\n"
@@ -41,26 +43,41 @@ class CollisionTestCase(unittest.TestCase):
         self.assertEqual(rc,0)
         self.assertEqual(out,correct_out)
         self.assertEqual(errs,"")        
+
+    #checking the final values after the time of collision
+    def test_post_collision(self):
+        strin="one 0 0 0 0\ntwo 12 0 -2 0"
+        correct_out="2\none -2 0 -2 0\ntwo 10 0 0 0\n"
+        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2"],strin)
+        self.assertEqual(rc,0)
+        self.assertEqual(out,correct_out)
+        self.assertEqual(errs,"")
+
+        # str1=strin[8:11]
+        # str2=strin[21:29]
+        # v1=[int(i) for i in str1.split()]
+        # v2=[int(i) for i in str2.split()]
+
+        # str3=strin[4:7]
+        # str4=string[15:21]
+        # x1=[int(i) for i in str3.split()]
+        # x2=[int(i) for i in str4.split()]
+
     
-#    def test_mult_col(self):  #fails 1, 7, 27
-#        strin = "one 20 10 -2 1\ntwo 0 0 3 3"
-#        correct_out = "3\none 14 13 -2 1\ntwo 9 9 3 3x\n"
-#        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["3"],strin)
-#        self.assertEqual(rc,0)
-#        self.assertEqual(out,correct_out)
-#        self.assertEqual(errs,"")        
-    
-    def input_test_letter(self):
+    #checking if the program gives an error when a character passed as time constraint
+    def test_input_letter(self):
         strin = "one 20 10 -2 1"
         (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["a"],strin)
         self.assertEqual(rc,2)
 
-    def input_test_space(self):
+    #checking if the program gives an error when no time contraint given
+    def test_input_space(self):
         strin = "one 20 10 -2 1"
         (rc,out,errs) = runprogram(PROGRAM_TO_TEST,[""],strin)
         self.assertEqual(rc,2) 
     
-    def partical_test_extra(self):
+    #checking if extra characters have been entered in the input
+    def test_extra(self):
         strin = "one 20 10 -2 1 1"
         (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["3"],strin)
         self.assertEqual(rc,1)
@@ -76,4 +93,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
