fbc89fb90a725377c7f044bcbcc7e99fe9afcba7
Keval Khara <kevalkhara95@gmail.com>
2017-10-10 21:31:52 -0400

    Update collision_tester.py

--- collision_tester.py
+++ collision_tester.py
@@ -1,5 +1,3 @@
-"""this is the main part of the assignment"""
-
 # Copyright 2017 Keval_Khara kevalk@bu.edu
 # Copyright 2017 Donato_Kava dkava@bu.edu
 # Copyright 2017 Harish_NS harishns@bu.edu
@@ -45,10 +43,46 @@ class CollisionTestCase(unittest.TestCase):
         self.assertEqual(errs,"")        
 
     #checking the final values after the time of collision
-    def test_post_collision(self):
+    def test_post_collision_xaxis(self):
         strin="one 0 0 0 0\ntwo 12 0 -2 0"
         correct_out="2\none -2 0 -2 0\ntwo 10 0 0 0\n"
         (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2"],strin)
+        self.assertEqual(rc,0)
+        self.assertEqual(out,correct_out)
+        self.assertEqual(errs,"")
+
+    #checking the final values after the time of collision
+    def test_post_collision_yaxis(self):
+        strin="one 0 0 0 0\ntwo 0 12 0 -2"
+        correct_out="2\none 0 -2 0 -2\ntwo 0 10 0 0\n"
+        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2"],strin)
+        self.assertEqual(rc,0)
+        self.assertEqual(out,correct_out)
+        self.assertEqual(errs,"")
+
+    #checking the output values after the time of collision
+    def test_pos_after_collision_xyaxis(self):
+        strin = "one 2 2 1 1\ntwo 12 10 -1 -1"
+        correct_out = "2\none 1.76 2.32 -1.24 -0.68\ntwo 12.24 9.68 1.24 0.68\n"
+        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2"],strin)
+        self.assertEqual(rc,0)
+        self.assertEqual(out,correct_out)
+        self.assertEqual(errs,"") 
+
+    #checking the output with big input values
+    def test_bigInput(self):
+        strin="one 100000 0 -100000 0"
+        correct_out="1\none 0 0 -100000 0\n"
+        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["1"],strin)
+        self.assertEqual(rc,0)
+        self.assertEqual(out,correct_out)
+        self.assertEqual(errs,"")
+
+    #checking the output with big time values
+    def test_bigTime(self):
+        strin="one 0 0 1 0"
+        correct_out="100000\none 100000 0 1 0\n"
+        (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["100000"],strin)
         self.assertEqual(rc,0)
         self.assertEqual(out,correct_out)
         self.assertEqual(errs,"")
@@ -63,6 +97,23 @@ class CollisionTestCase(unittest.TestCase):
         # x1=[int(i) for i in str3.split()]
         # x2=[int(i) for i in str4.split()]
 
+    # #checking the final values when all 3 objects moving  
+    # def test_pos_after_collision_three(self):
+    #     strin = "one 0 0 1 0\ntwo 12 0 -1 0\nthree -18 -18 0 -1"
+    #     correct_out = "2\none 0 0 -1 0\ntwo 12 0 1 0\nthree -18 -20 0 -1\n"
+    #     (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["2"],strin)
+    #     self.assertEqual(rc,0)
+    #     self.assertEqual(out,correct_out)
+    #     self.assertEqual(errs,"")   
+
+    # #checking the final values when 2 objects moving and 3rd is fixed
+    # def test_collision_then_Anothercollision_fixed(self):
+    #     strin = "one 0 0 1 0\ntwo 12 0 -1 0\nthree 24 0 0 0"
+    #     correct_out = "6\none -4 0 -1 0\ntwo 14 0 0 0\nthree 26 0 1 0\n"
+    #     (rc,out,errs) = runprogram(PROGRAM_TO_TEST,["6"],strin)
+    #     self.assertEqual(rc,0)
+    #     self.assertEqual(out,correct_out)
+    #     self.assertEqual(errs,"")        
     
     #checking if the program gives an error when a character passed as time constraint
     def test_input_letter(self):
